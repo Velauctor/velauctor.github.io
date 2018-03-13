9ae78490b3fe09a964e5dd3d211aa8fb44dea4e5
Ben Gamber <ben@stickfishstudio.com>
2018-03-13 11:36:13 -0400

    Blueprint for Authentication popup

---
+++ public/js/clock.js
@@ -0,0 +1,11 @@
+var timeSlot = $('.time');
+var greetSlot = $('.greeting');
+var greetList = ['Hello, ', 'Hi, ', 'Greetings, ', 'Salve, ', 'Sveiki, ', 'Guten tag, ', 'Dia dhuit, ', 'Kamusta, ', 'Aloha, ', 'Hola, ']
+var randomGreet = Math.floor(Math.random() * greetList.length);
+
+timeSlot.text(moment().format('LT'));
+greetSlot.text(greetList[randomGreet] + 'User');
+
+setInterval(function() {
+  timeSlot.text(moment().format('LT'));
+}, 5000);
