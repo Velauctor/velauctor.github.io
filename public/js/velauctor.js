76799acc413399fa9c65276492fc9f58cf7176ae
Ben Gamber <ben@stickfishstudio.com>
2018-03-13 16:56:48 -0400

    Re-integrated firebase authentication into main page

--- public/js/velauctor.js
+++ public/js/velauctor.js
@@ -1,49 +1,63 @@
-var signInOuts = document.querySelectorAll('.dom-sign-in'),
-  statuses = document.querySelectorAll('.dom-sign-in-status');
- var clockBox = document.querySelector('.clock'); 
- var loginPage = document.getElementById('login-page'),
- onboardingPage = document.getElementById('onboarding-page'),
- homePage = document.getElementById('home-page');
- var githubDisplay = document.querySelector('.githubDisplay'),
- trelloDisplay = document.querySelector('.trelloDisplay');
- var googleDisplayName = 'User';
-
- 
-
-
-// github info 
-var accessGitHub = function() {
-     var responseObj = JSON.parse(this.responseText);
-    // console.log(responseObj.name + " has " + responseObj.public_repos + " public Github repositories!");
-     
-         //console.log('Github LOGIN: '+ responseObj.login);
-         // for(var obj in responseObj) {
-         //     console.log(obj+": "+responseObj[obj]);
-         // }
-         console.log('Repo Names:');
-         var githubDisplayText = '';
-         var repoList = document.createElement('ul');
-         githubDisplay.appendChild(repoList);
-         for(var i=0;i<responseObj.length;i++) {
-             console.log(i+": "+responseObj[i].name);
-             githubDisplayText += '\n * '+responseObj[i].name;
-             var repoLine = document.createElement('li');
-             repoLine.textContent = responseObj[i].name;
-             repoList.appendChild(repoLine);
-         }
-         console.log('githubDisplay: '+ githubDisplayText);
+//   var signInOuts = document.querySelectorAll('.dom-sign-in'),
+//   statuses = document.querySelectorAll('.dom-sign-in-status');
+//  var clockBox = document.querySelector('.clock'); 
+//  var loginPage = document.getElementById('login-page'),
+//  onboardingPage = document.getElementById('onboarding-page'),
+//  homePage = document.getElementById('home-page');
+//  var githubDisplay = document.querySelector('.githubDisplay'),
+//  trelloDisplay = document.querySelector('.trelloDisplay');
+//  function clock() {// We create a new Date object and assign it to a variable called "theTime".
+//      var time = new Date(),
          
-        // githubDisplay.textContent = githubDisplayText;
- }
- var gitHubUserID = document.getElementById('github-namefield-submit').value;
- var request = new XMLHttpRequest();
- request.onload = accessGitHub;
- request.open('get', 'https://api.github.com/users/'+gitHubUserID+'/repos', true);
- request.onerror = function(){
-     console.log('The given gitHubUserID ('+gitHubUserID+') is not valid.');
- }
- request.send();
+//          // Access the "getHours" method on the Date object with the dot accessor.
+//          hours = time.getHours(),
+//          minutes = time.getMinutes(),
+//          seconds = time.getSeconds();
 
+//          clockBox.textContent = theTime(hours) + ":" + theTime(minutes) + ":" + theTime(seconds);
+     
+//      function theTime(standIn) {
+//          if (standIn < 10) {
+//          standIn = '0' + standIn
+//          }
+//          return standIn;
+//      }
+//  }
+//  setInterval(clock, 1000);
+
+
+// // github info 
+// var accessGitHub = function() {
+//      var responseObj = JSON.parse(this.responseText);
+//     // console.log(responseObj.name + " has " + responseObj.public_repos + " public Github repositories!");
+     
+//          //console.log('Github LOGIN: '+ responseObj.login);
+//          // for(var obj in responseObj) {
+//          //     console.log(obj+": "+responseObj[obj]);
+//          // }
+//          console.log('Repo Names:');
+//          var githubDisplayText = '';
+//          var repoList = document.createElement('ul');
+//          githubDisplay.appendChild(repoList);
+//          for(var i=0;i<responseObj.length;i++) {
+//              console.log(i+": "+responseObj[i].name);
+//              githubDisplayText += '\n * '+responseObj[i].name;
+//              var repoLine = document.createElement('li');
+//              repoLine.textContent = responseObj[i].name;
+//              repoList.appendChild(repoLine);
+//          }
+//          console.log('githubDisplay: '+ githubDisplayText);
+         
+//         // githubDisplay.textContent = githubDisplayText;
+//  }
+//  var gitHubUserID = 'kbooth1000';
+//  var request = new XMLHttpRequest();
+//  request.onload = accessGitHub;
+//  request.open('get', 'https://api.github.com/users/'+gitHubUserID+'/repos', true);
+//  request.onerror = function(){
+//      console.log('The given gitHubUserID ('+gitHubUserID+') is not valid.');
+//  }
+//  request.send();
 
 // Function called when clicking the Login/Logout button.
 // [START buttoncallback]
@@ -58,24 +72,24 @@ var accessGitHub = function() {
          // [START signin]
          firebase.auth().signInWithRedirect(provider);
 
-         [].forEach.call(signInOuts, function(signInOut) {
-             signInOut.disabled = false;
-         });
+        //  [].forEach.call(signInOuts, function(signInOut) {
+        //      signInOut.disabled = false;
+        //  });
          // [END signin]
      } else {
          // [START signout]
          firebase.auth().signOut();
          // [END signout]
-     [].forEach.call(signInOuts, function(signInOut) {
-             signInOut.disabled = true;
-         });
+    //  [].forEach.call(signInOuts, function(signInOut) {
+    //          signInOut.disabled = true;
+    //      });
      }
  }
- var activateHomePage = function(){
-    homePage.style.display = 'block';
-    loginPage.style.display = 'none';
-    onboardingPage.style.display = 'none';
- }
+//  var activateHomePage = function(){
+//     homePage.style.display = 'block';
+//     loginPage.style.display = 'none';
+//     onboardingPage.style.display = 'none';
+//  }
 
  var activateHomePage = function(){
     homePage.style.display = 'block';
@@ -135,45 +149,48 @@ var accessGitHub = function() {
              var uid = user.uid;
              var providerData = user.providerData;
 
-             [].forEach.call(statuses, function(status) {
-                 status.textContent = 'Signed in';
-             });
+            //  [].forEach.call(statuses, function(status) {
+            //      status.textContent = 'Signed in';
+            //  });
 
 
-             [].forEach.call(signInOuts, function(signInOut) {
-                 signInOut.textContent = 'Sign out';
-             });
+            //  [].forEach.call(signInOuts, function(signInOut) {
+            //      signInOut.textContent = 'Sign out';
+            //  });
 
              console.log('User account details: ' + JSON.stringify(user, null, '  '));
+            
+            $('.signin-block').toggleClass('hidden');
+            //  document.getElementById('home-page-greeting').textContent = 'Hi, ' + googleDisplayName;
+    
+            //  // Turn on the proper page
+            //  if(googleEmail === 'kbooth1000@gmail.com') {  // TEST WITH MY OWN TWO GMAIL ACCOUNTS (kbooth1000 and kjbooth1000)
+            //     activateHomePage();
+            //  } else //if(googleEmail === 'kjbooth1000@gmail.com') {
+            //      { onboardingPage.classList.add('active-page');
+            //      loginPage.style.display = 'none';
+            //      homePage.style.display = 'none';
+            //  }
+        //  } else {        // *******************  User is signed out.
+            //  [].forEach.call(statuses, function(status) {
+            //      status.textContent = 'Signed out';
+            //  });
 
-             document.getElementById('home-page-greeting').textContent = googleDisplayName;
-             
-            // Turn on the proper page
-            if(googleEmail === 'kbooth1000@gmail.com') {  // TEST WITH MY OWN TWO GMAIL ACCOUNTS (kbooth1000 and kjbooth1000)
-                activateHomePage();
-             } else //if(googleEmail === 'kjbooth1000@gmail.com') {
-                 { onboardingPage.classList.add('active-page');
-                 loginPage.style.display = 'none';
-                 homePage.style.display = 'none';
-             }
-         } else {        // *******************  User is signed out.
-             [].forEach.call(statuses, function(status) {
-                 status.textContent = 'Signed out';
-             });
+            //  [].forEach.call(signInOuts, function(signInOut) {
+            //      signInOut.textContent = 'Sign in with Google';
+            //  });
 
-             [].forEach.call(signInOuts, function(signInOut) {
-                 signInOut.textContent = 'Sign in with Google';
-             });
-
-             [].forEach.call(signInOuts, function(signInOut) {
-                 signInOut.disabled = false;
-             });
+            //  [].forEach.call(signInOuts, function(signInOut) {
+            //      signInOut.disabled = false;
+            //  });
          };
          // [END authstatelistener]
-         [].forEach.call(signInOuts, function(signInOut) { // watches the signin/out button one each page
-             signInOut.addEventListener('click', toggleSignIn, false);
-         });
-         document.getElementById('skip-setup').addEventListener('click', function(){ activateHomePage() ;console.log('Activate Home Page---------------'); });//
+        //  [].forEach.call(signInOuts, function(signInOut) { // watches the signin/out button one each page
+        //      signInOut.addEventListener('click', toggleSignIn, false);
+        //  });
+
+        //  document.getElementById('skip-setup').addEventListener('click', function(){ activateHomePage() ;console.log('Activate Home Page---------------'); });//
+           
 
          document.getElementById("github-namefield").addEventListener("keyup", function(event) {
             event.preventDefault();
@@ -183,6 +200,15 @@ var accessGitHub = function() {
         });
      });
  }
+
+$('.google-btn').on('click', e => {
+    toggleSignIn();
+});
+
+$('.signout').on('click', e => {
+    toggleSignIn();
+});
+
      window.onload = function() {
          initApp();
      };
